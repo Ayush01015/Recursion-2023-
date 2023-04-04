fb0abdc88fdcfc287f356b648f42efb28c57a4d5
Ayush Shrivastav <108419958+Ayush01015@users.noreply.github.com>
2023-04-04 14:22:07 +0530

    Reversing a Linked List Through Recursion

--- Linked List/ViewLL.cpp
+++ Linked List/ViewLL.cpp
@@ -1,134 +1,176 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-class node{
-    public:
-        int item;
-        node *next;
+class node
+{
+public:
+    int item;
+    node *next;
 };
-class SLL{
+class SLL
+{
     node *start;
-    public:
-        SLL();
-        void view();
-        void view(node*);
-        void view(node*s,int&); // view from last pos
-        void view(node*s,int&,int&); // view from last pos
-        void view(int); // view from last pos
-        void insertAtFirst(int);
-        void insertAtLast(int);
-        node* search(int);
-        void insertAfter(node*,int);
-        void deleteLastNode();
-        void deleteFirstNode();
-        void deleteNode(node*);
+
+public:
+    SLL();
+    void view();
+    void view(node *);
+    void view(node *s, int &); // view from last pos
+    void view(int);            // view from last pos
+    node* reverse(node*);
+    void reverse();
+    void insertAtFirst(int);
+    void insertAtLast(int);
+    node *search(int);
+    void insertAfter(node *, int);
+    void deleteLastNode();
+    void deleteFirstNode();
+    void deleteNode(node *);
 };
-SLL::SLL(){
+SLL::SLL()
+{
     start = NULL;
 }
-void SLL::view(node *r){
-    if(r==NULL)
+void SLL::view(node *r)
+{
+    if (r == NULL)
         return;
-    cout<<r->item<<" ";
+    cout << r->item << " ";
     view(r->next);
 }
-void SLL::view(){
-    if(start==NULL)
-        cout<<"List is Empty\n";
-    else{
+void SLL::view()
+{
+    if (start == NULL)
+        cout << "List is Empty\n";
+    else
+    {
         view(start);
     }
-    cout<<endl;
+    cout << endl;
 }
-void SLL::view(int pos){
-    if(start==NULL)
-        cout<<"List is Empty\n";
-    else{
-        view(start,pos);
+void SLL::view(int pos)
+{
+    if (start == NULL)
+        cout << "List is Empty\n";
+    else
+    {
+        view(start, pos);
     }
-    cout<<endl;
+    cout << endl;
 }
-void SLL::view(node *s,int &pos){ //pos node from last
-    if(s==NULL){
+void SLL::view(node *s, int &pos)
+{ // pos node from last
+    if (s == NULL)
+    {
         return;
     }
-    view(s->next,pos);
+    view(s->next, pos);
     pos--;
-    if(pos==0)
-        cout<<s->item<<" ";
+    if (pos == 0)
+        cout << s->item << " ";
 }
 
-
-void SLL::insertAtLast(int data){
+node* SLL::reverse(node *s)
+{
+    if (s == NULL || s->next == NULL)
+        return s;
+    node *n = reverse(s->next);
+    s->next->next = s;
+    s->next = NULL;
+    return n;
+}
+void SLL::reverse(){
+    if(start==NULL)
+        cout<<"List is Empty\n";
+    else
+        start = reverse(start);
+}
+void SLL::insertAtLast(int data)
+{
     node *n = new node();
     node *t = start;
     n->item = data;
     n->next = NULL;
-    if(start == NULL)
+    if (start == NULL)
         start = n;
-    else{
-        while(t->next!=NULL)
-            t=t->next;
+    else
+    {
+        while (t->next != NULL)
+            t = t->next;
         t->next = n;
     }
 }
-void SLL::insertAtFirst(int data){
+void SLL::insertAtFirst(int data)
+{
     node *n = new node();
     n->item = data;
     n->next = start;
-    start = n;   
+    start = n;
 }
-node* SLL::search(int data){
-    if(start == NULL)
-        cout<<"List is Empty\n";
-    else{
+node *SLL::search(int data)
+{
+    if (start == NULL)
+        cout << "List is Empty\n";
+    else
+    {
         node *t = start;
-        while(t){
-            if(t->item==data)
+        while (t)
+        {
+            if (t->item == data)
                 return t;
-            t=t->next;
+            t = t->next;
         }
     }
-    cout<<"Data Not Found\n";
+    cout << "Data Not Found\n";
     return NULL;
 }
-void SLL::insertAfter(node *r,int data){
+void SLL::insertAfter(node *r, int data)
+{
     node *n = new node();
     n->item = data;
     n->next = r->next;
     r->next = n;
 }
-void SLL::deleteLastNode(){
-    if(start == NULL)
-        cout<<"List is Empty\n";
-    else{
+void SLL::deleteLastNode()
+{
+    if (start == NULL)
+        cout << "List is Empty\n";
+    else
+    {
         node *t = start, *t1 = NULL;
-        while(t->next!=NULL){
+        while (t->next != NULL)
+        {
             t1 = t;
             t = t->next;
         }
-        if(t1!=NULL)
+        if (t1 != NULL)
             t1->next = NULL;
         else
             start = NULL;
         delete t;
     }
 }
-void SLL::deleteFirstNode(){
-    if(start == NULL)
-        cout<<"List is Empty\n";
-    else{
+void SLL::deleteFirstNode()
+{
+    if (start == NULL)
+        cout << "List is Empty\n";
+    else
+    {
         node *t = start;
         start = t->next;
         delete t;
     }
 }
-void SLL::deleteNode(node *r){
+void SLL::deleteNode(node *r)
+{
     node *t = start;
-    if(start == r){
+    if (start == r)
+    {
         start = r->next;
-    }else{
-        while(t->next!=r)
-            t=t->next;
+    }
+    else
+    {
+        while (t->next != r)
+            t = t->next;
         t->next = r->next;
     }
     delete r;
@@ -141,7 +183,10 @@ int main()
     l.insertAtLast(9);
     l.insertAtLast(8);
     l.insertAtLast(888);
-    l.view();  //All Nodes
-    l.view(1); //node from last
+    l.view(); // All Nodes
+    // l.view(1); //node from last
+    l.reverse();
+    l.view();
+
     return 0;
 }
